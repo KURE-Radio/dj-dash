6c2b90c6f48e6ef4b7bc62a8489f8929bd382a39
Michael Yeager <michaelyeager@icloud.com>
2013-07-30 20:51:53 -0500

    Beginning framework of unit tests.

---
+++ Tests/test-dj-dash-controllers/test-PlaylistCtrl.js
@@ -0,0 +1,61 @@
+describe("Playlist View Controller", function() {
+    var testTrack;
+    var testBreak;
+    var testPlaylist;
+    var testBreakOptions;
+
+    beforeEach(function() {
+        testTrack = {
+            track_number: 42,
+            is_track: true,
+            in_edit: null,
+            title: "Test Track Title",
+            artist: "Test Artist Name",
+            album: "Test Album Name"
+        };
+
+        testBreak = {
+            is_track: false,
+            title: "Test Break"
+        };
+
+        testPlaylist = [
+            {
+                track_number: 1,
+                is_track: true,
+                in_edit: null,
+                title: "Ends Of The Earth",
+                artist: "Lord Huron",
+                album: "Lonesome Dreams"
+            },
+            {
+                track_number: 2,
+                is_track: true,
+                in_edit: null,
+                title: "Bubblegum Trash",
+                artist: "Crocodiles",
+                album: "Endless Flowers"
+            },
+            {
+                track_number: 3,
+                is_track: true,
+                in_edit: null,
+                title: "Shades Of Blue",
+                artist: "Gregory Alan Isakov",
+                album: "Live from KURE"
+            }
+        ];
+
+        testBreakOptions = {
+            id: null,
+            event: null,
+            grant: null,
+            weather: null,
+            gsb: null,
+            psa: null,
+            notes: null
+        };
+    });
+
+    
+});
