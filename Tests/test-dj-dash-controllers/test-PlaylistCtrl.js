0a4a1e7567bd891082ba045331d312ee11280792
Michael Yeager <michaelyeager@icloud.com>
2013-08-08 00:21:16 -0500

    Adding unit tests for Playlist Controller.

--- Tests/test-dj-dash-controllers/test-PlaylistCtrl.js
+++ Tests/test-dj-dash-controllers/test-PlaylistCtrl.js
@@ -1,5 +1,9 @@
+var testTargetControllers = angular.module('dj-dash.controllers');
+var testTargetServices = angular.module('dj-dash.services');
+
 describe("Playlist View Controller", function() {
     var $scope;
+    var $playlist;
     var controller;
     var testTrack;
     var testBreak;
@@ -9,7 +13,10 @@ describe("Playlist View Controller", function() {
     beforeEach(function() {
         $scope = {};
 
-        controller = $controller('PlaylistCtrl', { $scope: $scope });
+        $playlist = testTargetServices('$playlist');
+        spyOn($playlist, 'getState');
+
+        controller = testTargetControllers('PlaylistCtrl', { $scope: $scope });
 
         testTrack = {
             track_number: 42,
@@ -64,6 +71,166 @@ describe("Playlist View Controller", function() {
     });
 
     it("is created with the correct state", function() {
-       spyOn('getState');
+        expect($playlist.getState).toHaveBeenCalledWith(null);
+        expect($scope.track_list).toEqual([]);
+        expect($scope.submission_state).toBe(false);
+        expect($scope.break_dialog_display_options).toBe(
+            {
+                backdrop: true,
+                backdropFade: true,
+                dialogFade: true,
+                templateUrl: "break_dialog.html",
+                controller: "AddBreakDialogCtrl"
+            }
+        );
+    });
+
+    describe("ShowEditButton Function", function() {
+        var track;
+
+        beforeEach(function () {
+            track = {
+                is_track: null,
+                in_edit: null
+            }
+        });
+
+        it("should not show on a break when not in edit", function() {
+            track.is_track = false;
+            track.in_edit = false;
+
+            expect($scope.showEditButton(track)).toBe(false);
+        });
+
+        it("should not show on a break when in edit", function() {
+            track.is_track = false;
+            track.in_edit = true;
+
+            expect($scope.showEditButton(track)).toBe(false);
+        });
+
+        it("should show on a track when not in edit", function() {
+            track.is_track = true;
+            track.in_edit = false;
+
+            expect($scope.showEditButton(track)).toBe(true);
+        });
+
+        it("should not show on a track when in edit", function() {
+            track.is_track = true;
+            track.in_edit = true;
+
+            expect($scope.showEditButton(track)).toBe(false);
+        });
+    });
+
+    describe("ShowConfirmButton Function", function() {
+        var track;
+
+        beforeEach(function() {
+            track = {
+                is_track: null,
+                in_edit: null
+            }
+        });
+
+        it("should not show on a break when not in edit", function() {
+            track.is_track = false;
+            track.in_edit = false;
+
+            expect($scope.showConfirmButton(track)).toBe(false);
+        });
+
+        it("should not show on a break when in edit", function() {
+            track.is_track = false;
+            track.in_edit = true;
+
+            expect($scope.showConfirmButton(track)).toBe(false);
+        });
+
+        it("should not show on a track when not in edit", function() {
+            track.is_track = true;
+            track.in_edit = false;
+
+            expect($scope.showConfirmButton(track)).toBe(false);
+        });
+
+        it("should show on a track when in edit", function() {
+            track.is_track = true;
+            track.in_edit = true;
+
+            expect($scope.showConfirmButton(track)).toBe(true);
+        });
+    });
+
+    describe("ShowTrackTitle Function", function() {
+        var track;
+
+        beforeEach(function () {
+            track = {
+                is_track: null,
+                in_edit: null
+            }
+        });
+
+        it("should not show on a break when not in edit", function () {
+            track.is_track = false;
+            track.in_edit = false;
+
+            expect($scope.showTrackTitle(track)).toBe(false);
+        });
+
+        it("should not show on a break when in edit", function () {
+            track.is_track = false;
+            track.in_edit = true;
+
+            expect($scope.showTrackTitle(track)).toBe(false);
+        });
+
+        it("should show on a track when not in edit", function () {
+            track.is_track = true;
+            track.in_edit = false;
+
+            expect($scope.showTrackTitle(track)).toBe(true);
+        });
+
+        it("should not show on a track when in edit", function () {
+            track.is_track = true;
+            track.in_edit = true;
+
+            expect($scope.showTrackTitle(track)).toBe(false);
+        });
+    });
+
+    describe("AddTrack Function", function() {
+
+    });
+
+    describe("RemoveTrack Function", function() {
+
+    });
+
+    describe("ClearTrackInEdit Function", function() {
+
+    });
+
+    describe("AddBreak Function", function() {
+
+    });
+
+    describe("OpenAddBreakDialog Function", function() {
+
+    });
+
+    describe("SubmitList Function", function() {
+
+    });
+
+    describe("GetNextTrackNumber Function", function() {
+
+    });
+
+    describe("CancelPreviousEdits Function", function() {
+
     });
 });
