ffbf13e5c20cce6553b14ae15a0cdcd3ce7b8137
Trevin Ward <tedger@gmail.com>
2013-10-01 18:13:18 -0500

    dbquery-post.php POST version of previous version dbquery.php

---
+++ ajs/dbquery-post.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: tedger
+ * Date: 9/28/13
+ * Time: 6:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**Array will eventually be queried from MSQL*/
+$states = array("Iowa","Minnesota","Idaho","Illinois","Michigan");
+$requesting = $_POST["entry"];
+$requesting = json_decode($requesting);
+$return= array();
+/** json_decode(file_get_contents("php://input"));*/
+foreach ($states as $state)
+{
+    if(is_string( strstr(strtolower($state), strtolower($requesting)))){
+        $return[] = $state;
+    }
+}
+
+echo json_encode($return);
+
+?>
